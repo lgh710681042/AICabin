5b228695a3be5f0d91da690aaec59d6f9fa55a2c
危波 <860813@nd.com>
2019-12-24 23:43:51 +0800

    修改查看成绩界面的ui及调用功能

--- AICabin/AICabin/AICabinWnd.cpp
+++ AICabin/AICabin/AICabinWnd.cpp
@@ -215,7 +215,7 @@ LRESULT CAICabinWnd::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
             CAIViewResultsWnd* pAIViewResultsWnd = new CAIViewResultsWnd;
             if (pAIViewResultsWnd)
             {
-                pAIViewResultsWnd->SetViewResultsState(Resultsstate_Zero);
+                pAIViewResultsWnd->SetPercentage(0, 30);
                 pAIViewResultsWnd->CreateWnd(GetHWND());
                 pAIViewResultsWnd->ShowWindow();
             }
@@ -226,7 +226,7 @@ LRESULT CAICabinWnd::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
             CAIViewResultsWnd* pAIViewResultsWnd = new CAIViewResultsWnd;
             if (pAIViewResultsWnd)
             {
-                pAIViewResultsWnd->SetViewResultsState(Resultsstate_Other);
+                pAIViewResultsWnd->SetPercentage(35, 70);
                 pAIViewResultsWnd->CreateWnd(GetHWND());
                 pAIViewResultsWnd->ShowWindow();
             }
@@ -237,7 +237,7 @@ LRESULT CAICabinWnd::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
             CAIViewResultsWnd* pAIViewResultsWnd = new CAIViewResultsWnd;
             if (pAIViewResultsWnd)
             {
-                pAIViewResultsWnd->SetViewResultsState(Resultsstate_Hundred);
+                pAIViewResultsWnd->SetPercentage(100, 99);
                 pAIViewResultsWnd->CreateWnd(GetHWND());
                 pAIViewResultsWnd->ShowWindow();
             }
--- AICabin/AICabin/AIViewResultsWnd.cpp
+++ AICabin/AICabin/AIViewResultsWnd.cpp
@@ -4,6 +4,19 @@
 
 CAIViewResultsWnd::CAIViewResultsWnd()
 {
+    m_nScore = 0;
+    m_strUseTime = _T("00:00:00");
+
+    m_pBtnReturn = nullptr;
+    m_pBtnLeave = nullptr;
+    m_pCtrlPaint50 = nullptr;
+    m_pCtrlPaint100 = nullptr;
+    m_pCtrlIcon = nullptr;
+    m_pLabelPercentage = nullptr;
+    m_pLabelTimeData = nullptr;
+    m_pLabelEncourage = nullptr;
+    m_pLayLeftBtnPanel = nullptr;
+    m_pLayRightBtnPanel = nullptr;
 }
 
 
@@ -37,33 +50,18 @@ HWND CAIViewResultsWnd::CreateWnd(HWND hParent)
 
 void CAIViewResultsWnd::OnCreate()
 {
-    CLayoutUI* pLayZero = dynamic_cast<CLayoutUI*>(FindControl(_T("AIViewResultsLayoutZero")));
-    CLayoutUI* pLayHundred = dynamic_cast<CLayoutUI*>(FindControl(_T("AIViewResultsLayoutHundred")));
-    CLayoutUI* pLayOther = dynamic_cast<CLayoutUI*>(FindControl(_T("AIViewResultsLayoutOther")));
-    if (!pLayZero || !pLayHundred || !pLayOther)
-        return;
+    m_pBtnReturn = dynamic_cast<CButtonUI*> (FindControl(_T("btn_view_results_return")));
+    m_pBtnLeave = dynamic_cast<CButtonUI*> (FindControl(_T("btn_view_results_leave")));
+    m_pCtrlPaint50 = dynamic_cast<CControlUI*> (FindControl(_T("ctrl_panel_paint_50")));
+    m_pCtrlPaint100 = dynamic_cast<CControlUI*> (FindControl(_T("ctrl_panel_paint_100")));
+    m_pCtrlIcon = dynamic_cast<CControlUI*> (FindControl(_T("ctrl_icon")));
+    m_pLabelPercentage = dynamic_cast<CAutoSizeButtonUI*> (FindControl(_T("percentage_label")));
+    m_pLabelTimeData = dynamic_cast<CAutoSizeButtonUI*> (FindControl(_T("timedata_label")));
+    m_pLabelEncourage = dynamic_cast<CAutoSizeButtonUI*> (FindControl(_T("encourage_label")));
+    m_pLayLeftBtnPanel = dynamic_cast<CLayoutUI*> (FindControl(_T("AILeftBtnPanel")));
+    m_pLayRightBtnPanel = dynamic_cast<CLayoutUI*> (FindControl(_T("AIRightBtnPanel")));
 
-    if (m_eState == Resultsstate_Zero)
-    {
-        pLayZero->SetVisible(true);
-        pLayHundred->SetVisible(false);
-        pLayOther->SetVisible(false);
-    }
-    else if (m_eState == Resultsstate_Hundred)
-    {
-        pLayZero->SetVisible(false);
-        pLayHundred->SetVisible(true);
-        pLayOther->SetVisible(false);
-    }
-    else
-    {
-        pLayZero->SetVisible(false);
-        pLayHundred->SetVisible(false);
-        pLayOther->SetVisible(true);
-    }
-
-   // GetRoot()->Resize();
-   // GetRoot()->Invalidate();
+    UpdateViewData();
 
 	__super::OnCreate();
 }
@@ -71,4 +69,148 @@ void CAIViewResultsWnd::OnCreate()
 void CAIViewResultsWnd::OnClose()
 {
 
+}
+
+bool CAIViewResultsWnd::OnEventReturn(TNotifyUI* pTNotify)
+{
+    return true;
+}
+
+bool CAIViewResultsWnd::OnEventLeave(TNotifyUI* pTNotify)
+{
+    CloseWindow();
+    return true;
+}
+
+bool CAIViewResultsWnd::OnEventNo(TNotifyUI* pTNotify)
+{
+    return true;
+}
+
+bool CAIViewResultsWnd::OnEventGo(TNotifyUI* pTNotify)
+{
+    return true;
+}
+
+void CAIViewResultsWnd::SetPercentage(int nScore, int nUseTime)
+{
+    m_strUseTime = SecondsToTimeString(nUseTime);
+    m_nScore = nScore;
+}
+
+void CAIViewResultsWnd::UpdateViewData()
+{
+    if (m_nScore >= 100)
+        ShowCtrlStatusOfHundred();
+    else if (m_nScore > 0)
+        ShowCtrlStatusOfOther();
+    else
+        ShowCtrlStatusOfZero();
+
+    if (m_pLabelPercentage)
+    {
+        TCHAR szValue[MAX_PATH];
+        _stprintf_s(szValue, _T("%d%%"), m_nScore);
+        m_pLabelPercentage->SetText(szValue);
+    }
+
+    if (m_pLabelTimeData)
+        m_pLabelTimeData->SetText(m_strUseTime.c_str());
+
+    GetRoot()->Resize();
+}
+void CAIViewResultsWnd::ShowCtrlStatusOfZero()
+{
+    if (m_pBtnReturn)
+        m_pBtnReturn->SetVisible(true);
+
+    if (m_pBtnLeave)
+        m_pBtnLeave->SetVisible(true);
+
+    if (m_pCtrlPaint50)
+        m_pCtrlPaint50->SetVisible(false);
+
+    if (m_pCtrlPaint100)
+        m_pCtrlPaint100->SetVisible(false);
+
+    if (m_pLayLeftBtnPanel)
+        m_pLayLeftBtnPanel->SetVisible(false);
+
+    if (m_pLayRightBtnPanel)
+        m_pLayRightBtnPanel->SetVisible(false);
+
+    //修改
+    if (m_pLabelEncourage)
+        m_pLabelEncourage->SetText(I18NSTR(_T("#StrNotGood")));
+
+    if (m_pCtrlIcon)
+        m_pCtrlIcon->SetAttribute(_T("bk.image"), _T("#view_results_icon_0"));
+}
+
+void CAIViewResultsWnd::ShowCtrlStatusOfOther()
+{
+    if (m_pBtnReturn)
+        m_pBtnReturn->SetVisible(true);
+
+    if (m_pBtnLeave)
+        m_pBtnLeave->SetVisible(true);
+
+    if (m_pCtrlPaint50)
+        m_pCtrlPaint50->SetVisible(true);
+
+    if (m_pCtrlPaint100)
+        m_pCtrlPaint100->SetVisible(false);
+
+    if (m_pLayLeftBtnPanel)
+        m_pLayLeftBtnPanel->SetVisible(false);
+
+    if (m_pLayRightBtnPanel)
+        m_pLayRightBtnPanel->SetVisible(false);
+
+    //修改
+    if (m_pLabelEncourage)
+        m_pLabelEncourage->SetText(I18NSTR(_T("#StrComplete")));
+
+    if (m_pCtrlIcon)
+        m_pCtrlIcon->SetAttribute(_T("bk.image"), _T("#view_results_icon_50"));
+}
+
+void CAIViewResultsWnd::ShowCtrlStatusOfHundred()
+{
+    if (m_pBtnReturn)
+        m_pBtnReturn->SetVisible(false);
+
+    if (m_pBtnLeave)
+        m_pBtnLeave->SetVisible(false);
+
+    if (m_pCtrlPaint50)
+        m_pCtrlPaint50->SetVisible(false);
+
+    if (m_pCtrlPaint100)
+        m_pCtrlPaint100->SetVisible(true);
+
+    if (m_pLayLeftBtnPanel)
+        m_pLayLeftBtnPanel->SetVisible(true);
+
+    if (m_pLayRightBtnPanel)
+        m_pLayRightBtnPanel->SetVisible(true);
+
+    //修改
+    if (m_pLabelEncourage)
+        m_pLabelEncourage->SetText(I18NSTR(_T("#StrComplete")));
+
+    if (m_pCtrlIcon)
+        m_pCtrlIcon->SetAttribute(_T("bk.image"), _T("#view_results_icon_100"));
+}
+
+std::wstring CAIViewResultsWnd::SecondsToTimeString(int nSeconds)
+{
+    int nHour = nSeconds / 3600;
+    int nMin = nSeconds % 3600 / 60;
+    int nSec = nSeconds % 60; //不足60的就是秒，够60就是分
+
+    TCHAR szTime[MAX_PATH];
+    _stprintf_s(szTime, _T("%02d:%02d:%02d"), nHour, nMin, nSec);
+
+    return std::wstring(szTime);
 }
